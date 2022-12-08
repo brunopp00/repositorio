30e0ed9877ad068442988a76970c04d9956bbb69
bruno <brunof@unimedvaledocai.com.br>
2022-12-08 10:37:27 -0300

    Alteração

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
+    const [repositorios, setRepositorios] = useState([])
+
+        
+        
+        fetch('https://api.github.com/users/brunopp00/repos').then(async res => {
+            if(!res.ok){
+                throw new Error(res.status)
+            }
+
+            setRepositorios(await res.json())
+
+        }).catch(e => console.log('e',e))
+        
+        return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='navbar navbar-expand-md navbar-dark bg-dark mb-4'>
+            <a className='navbar-brand' href='https://github.com/brunopp00' target='_blank' rel="noreferrer">Bruno Frohlich</a>
+      </nav>
+      <main className='container'>
+        <div className='jumbutron'>
+            <h1>Veja meus repositórios</h1>
+            <p className='lead'>
+                Programas criados por mim:
+            </p>
+            {repositorios.map(repositorio => 
+            <div className='card col-md-12'>
+                    <div className='card-body'>
+                        <h1>{repositorio.name}</h1>
+                        <a target='_blank' href={repositorio.html_url} rel="noreferrer">{repositorio.html_url}</a>
+                    </div>
+                </div>
+                )}
+        </div>
+      </main>
     </div>
   );
 }
